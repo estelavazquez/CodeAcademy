31bda6774a358d22883cdfce10d627700f7ada57
Estela <105049053+estelavazquez@users.noreply.github.com>
2022-08-24 20:17:18 +0200

    Create Tables.sql

---
+++ SQL/Tables.sql
@@ -0,0 +1,23 @@
+/* Tables - Estela Vázquez Paretas */
+CREATE TABLE enemies (
+id INTEGER,
+name TEXT,
+birthday DATE
+);
+
+INSERT INTO enemies (id, name, birthday)
+VALUES (1, 'Ororo Munroe', '30/05/1940');
+
+INSERT INTO enemies (id, name, birthday)
+VALUES 
+  (2, 'Pedro', '3-12-2002'),
+  (3, 'David', '4-2-200');
+
+ALTER TABLE enemies
+ADD COLUMN email TEXT;
+
+DELETE FROM enemies
+WHERE id = 1;
+
+select *
+FROM enemies;
