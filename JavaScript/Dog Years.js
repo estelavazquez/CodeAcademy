f9ae601592b796de80f3b39ecd1996617e3c260c
Estela <105049053+estelavazquez@users.noreply.github.com>
2022-08-24 20:13:54 +0200

    Create Dog Years.js

---
+++ JavaScript/Dog Years.js
@@ -0,0 +1,12 @@
+/* Dog Years - Estela Vázquez Paretas */
+//My currently age
+const myAge = 25;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+//This is my dog age
+let myAgeInDogYears = earlyYears + laterYears;
+//The next method returns a string with a lowercase letters
+const myName = 'Juan'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
